d91f803e151bdcf2b860992a5b7c428831dc0d5d
Micah Mosley <micah.mosley@utexas.edu>
2020-06-25 20:31:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   counter=0 
-  while counter<array.length {
-  array[counter]=array[counter]**2
-  counter+=1
-  }
-end
+  while counter<array.length 
+    array[counter]=array[counter]**2
+    counter+=1
+  end
 end
